b1e0ab816a29f057f8bd21bb8ab5e5fcd1780348
PyEZIO <151058514+elimsi@users.noreply.github.com>
2025-04-04 12:17:00 +0100

    Setup basic board matrix

--- game_engine.py
+++ game_engine.py
@@ -3,3 +3,8 @@ import numpy as np
 EMPTY = 0
 BLACK = 1
 WHITE = 2
+DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+class OthelloGame:
+    def __init__(self):
+        self.board = np.zeros((8, 8), dtype=int)
