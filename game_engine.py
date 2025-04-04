558601e19b53774b28f48d412e2ab64c738ad18c
PyEZIO <151058514+elimsi@users.noreply.github.com>
2025-04-04 14:34:00 +0100

    Add piece flipping logic

--- game_engine.py
+++ game_engine.py
@@ -8,3 +8,8 @@ DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 
 class OthelloGame:
     def __init__(self):
         self.board = np.zeros((8, 8), dtype=int)
+        self.board[3, 3] = self.board[4, 4] = WHITE
+        self.board[3, 4] = self.board[4, 3] = BLACK
+        self.current_player = BLACK
+
+    def get_opponent(self, player):
