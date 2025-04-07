5214a2bc9632179fd2d6b741a3cd4747bb9a9adf
PyEZIO <151058514+elimsi@users.noreply.github.com>
2025-04-07 18:58:00 +0100

    Implement Greedy Heuristic AI

--- ai_heuristics.py
+++ ai_heuristics.py
@@ -8,3 +8,13 @@ def dumbScore(array, player):
     for x in range(8):
         for y in range(8):
             if array[x][y] == colour:
+                score += 1
+            elif array[x][y] == opponent:
+                score -= 1
+    return score
+
+def slightlyLessDumbScore(array, player):
+    score = 0
+    colour = player
+    opponent = 1 if player == 2 else 2
+
