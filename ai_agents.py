adbb037f846520f596141cf922a93552424e0928
PyEZIO <151058514+elimsi@users.noreply.github.com>
2025-04-10 16:56:00 +0100

    Add nodes explored counter

--- ai_agents.py
+++ ai_agents.py
@@ -16,3 +16,12 @@ class RandomAI(AIAgent):
             return random.choice(moves)
         return None
 
+class HeuristicAI(AIAgent):
+    # Uses slightlyLessDumbScore to greedily pick the best immediate move (1-ply)
+    def get_move(self, game_state):
+        moves = game_state.get_valid_moves(game_state.board, self.color)
+        if not moves: return None
+        best_score = float('-inf')
+        best_move = None
+        for r, c in moves:
+            new_board = game_state.apply_move(game_state.board, self.color, r, c)
